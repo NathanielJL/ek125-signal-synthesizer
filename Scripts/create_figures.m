955bdc0071591efa7567915382f750d72bf0d487
kwajler-ship-it <kwajler@bu.edu>
2026-04-13 01:58:08 -0400

    runs great!

--- Scripts/create_figures.m
+++ Scripts/create_figures.m
@@ -3,8 +3,6 @@
 % Description:
 %   Generates all required figures and saves them as PNG files
 
-run('Scripts/Visualization/create_figures.m')
-
 clear; clc; close all;
 
 % Create Figures folder if it doesn't exist
